9f36d653888a8870e575afcd62e7b2e9c5386d5c
ahmodhasokawaii <ahmed.saleh292010@gmail.com>
2026-03-04 02:50:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.set("trust proxy", true);
+
+app.get("/", (req, res) => {
+    let userIP = req.ip;
+
+    // Convert ::ffff:127.0.0.1 style IPv4 to normal
+    if (userIP.startsWith("::ffff:")) {
+        userIP = userIP.replace("::ffff:", "");
+    }
+
+    console.log("Visitor IPv4:", userIP);
+    res.send("Your IPv4 is: " + userIP);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
